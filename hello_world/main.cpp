eb9e9c416303f1f538a36026dd0fe68a77ba6554
Melanie Meléndez <57066220+MelanieRD@users.noreply.github.com>
2025-09-20 00:15:28 -0400

    hello world

---
+++ hello_world/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>  // Libreria de entrada y salida
+using namespace std;  // namespace = paquetes en java. aqui estamos usando el estandar dodne esta sout para imprimir y mas 
+
+int main(){
+    count << "Hola mundo" << endl;
+    return 0;
+}
